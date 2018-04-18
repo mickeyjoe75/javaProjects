a472e793f311b7e5b0dc86dfac521ecbf96397e9
Joseph Manuel <mickeyjoe75@gmail.com>
2018-04-18 13:59:03 +0100

    lab start point

---
+++ composition/src/main/java/wizard_management/beasts/Dragon.java
@@ -0,0 +1,16 @@
+package wizard_management.beasts;
+
+
+import wizard_management.behaviours.IFlyable;
+
+public class Dragon extends MythicalBeast implements IFlyable {
+
+    public Dragon(String name){
+        super(name);
+    }
+
+    public String fly(){
+        return "Standing up tall, beating wings, lift off!";
+    }
+
+}
---
+++ composition/src/main/java/wizard_management/cleaning/Broomstick.java
@@ -0,0 +1,22 @@
+package wizard_management.cleaning;
+
+
+import wizard_management.behaviours.IFlyable;
+
+public class Broomstick extends CleaningImplement implements IFlyable {
+
+    int speed;
+
+    public Broomstick(String brand, int speed){
+        super(brand);
+        this.speed = speed;
+    }
+
+    public int getSpeed(){
+        return this.speed;
+    }
+
+    public String fly(){
+        return "mounting broom, running, skipping, flying!";
+    }
+}
