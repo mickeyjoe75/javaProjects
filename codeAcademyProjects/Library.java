e8e0201ab8f95352fe372858025d6cea1ee2992f
Joseph Manuel <mickeyjoe75@gmail.com>
2018-04-23 00:51:35 +0100

    added A simple java proram that prints out the completion staus of books incorporating the Hashmap funtion in Java

---
+++ codeAcademyProjects/Library.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+//A simple java proram that prints out the completion staus of books incorporating the Hashmap funtion in Java....
+public class Library{
+
+  public Library(){
+
+  }
+
+  public void getFinishedBooks(HashMap<String, Boolean> library){
+
+    if (library.size() < 1){
+      System.out.println("Caustion!! HashMap is Empty!");
+    }
+    else{
+      for(String book : library.keySet()){
+        if(library.get(book) == true){
+          System.out.println(book);
+        }
+      }
+    }
+  }
+  public void getIncompleteBooks(HashMap<String, Boolean> library){
+
+    if (library.size() < 1){
+      System.out.println("Caustion!! HashMap is Empty!");
+    }
+    else{
+      for(String book : library.keySet()){
+        if(library.get(book) == false){
+          System.out.println(book);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args){
+
+    HashMap <String, Boolean> myBooks = new HashMap<String, Boolean>();
+
+    myBooks.put("Road Down The Funnel", true);
+    myBooks.put("Rat: A Biology", false);
+    myBooks.put("TimeIn", true);
+    myBooks.put("3D Food Printing", false);
+
+    Library myLibrary = new Library();
+    myLibrary.getFinishedBooks(myBooks);
+    myLibrary.getIncompleteBooks(myBooks);
+
+  }
+
+}
